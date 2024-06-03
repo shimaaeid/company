3fa2dbb887a1250f63314fb8ad38d798c1159c68
shimaa eid <shimaae717@gmail.com>
2024-06-03 19:47:51 +0300

    fix indexes

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -13,13 +13,13 @@ import { PortofolioDetailComponent } from './portfolio-detail/portfolio-detail.c
 
 
 const routes: Routes = [
+  // {
+  //   path: '',
+  //   redirectTo: '/',
+  //   pathMatch: 'full'
+  // },
   {
     path: '',
-    redirectTo: 'index',
-    pathMatch: 'full'
-  },
-  {
-    path: 'index',
     component: IndexComponent
   },
   {
